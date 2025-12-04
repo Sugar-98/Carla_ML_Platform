3d353c1085dd4fa6715bada2f01c9cec5a2ef4be
Sugar-98 <154723489+Sugar-98@users.noreply.github.com>
2025-12-04 22:18:36 +0000

    Modify docker build script

--- setup/build_image.sh
+++ setup/build_image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 IMAGE_NAME="CARLA_ML_PLATFORM"
 TAG="latest"
+Dockerfile="setup.Dockerfile"
 
-docker build -t ${IMAGE_NAME}:${TAG} .
+docker build -t ${IMAGE_NAME}:${TAG} -f ${Dockerfile} .

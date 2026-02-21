0b5bb9577a29b52077d91dd729f7d6d2dca639a9
Sugar-98 <154723489+Sugar-98@users.noreply.github.com>
2026-02-21 00:58:59 +0000

    train: 2026-02-16_15-18-40

--- LiftSplatShoot/Analysis/Visualize.sh
+++ LiftSplatShoot/Analysis/Visualize.sh
@@ -4,9 +4,9 @@
 source ../../env.sh #Define global path settings
 
 data_path="$PROJECT_ROOT/logs/dataset_plot"
-pretrained_model_dir="$PROJECT_ROOT/pretrained_models/LSS/2026-01-31_02-22-05" #Path for pretrained model. 
-state_dict_file='model_latest.pth'  #Pretrained model file
-save_path="$PROJECT_ROOT/pretrained_models/LSS/2026-01-31_02-22-05/visualize.mp4"
+pretrained_model_dir="$PROJECT_ROOT/pretrained_models/LSS/" #Path for pretrained model. 
+state_dict_file='model_best.pth'  #Pretrained model file
+save_path="$PROJECT_ROOT/pretrained_models/LSS/visualize.mp4"
 
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/analyze_model.py" #Define train script
 
